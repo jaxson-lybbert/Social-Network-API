85c5882161bc1dc44b341c7acaf404861776bd04
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-07-14 17:19:47 -0400

    Built out models

---
+++ models/Thought.js
@@ -0,0 +1,55 @@
+const { Schema } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      timestamps: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: reactionSchema,
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: new ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    timestamps: true,
+  },
+});
+
+module.exports = { thoughtSchema, reactionSchema };
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/,
+    },
+    thoughts: [Thought],
+    friends: [User],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+module.exports = userSchema;
