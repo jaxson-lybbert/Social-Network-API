c1cc9165d6dad489504d639edc66cbcc303483ab
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-07-17 12:06:49 -0400

    Added PUT route to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
+  // Gets all users in the database
   async getAllUsers(req, res) {
     try {
       const users = await User.find();
@@ -10,6 +11,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Gets a single user by ID
   async getUserById(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.id });
@@ -23,6 +25,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Creates a new user
   async createUser(req, res) {
     try {
       const newUser = await User.create(req.body);
@@ -32,13 +35,26 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Updates an existing user
   async updateUser(req, res) {
     try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!updatedUser) {
+        return res.status(404).json({ message: "No user with this id!" });
+      }
+
+      res.json(updatedUser);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
+  // Deletes an existing user and all associated thoughts
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndDelete({ _id: req.params.id });
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,11 +3,12 @@ const {
   getAllUsers,
   getUserById,
   createUser,
+  updateUser,
   deleteUser,
 } = require("../../controllers/userController");
 
 router.route("/").get(getAllUsers).post(createUser);
 
-router.route("/:id").get(getUserById).delete(deleteUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
